cc1de9e3eb6e06c337852ae134a4053e1a1d1c23
Jonas Le Duff <jonas.leduff@etu.unicaen.fr>
2024-10-02 11:27:44 +0200

    transmission of the api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let select = document.querySelector('#selectCommune');
-
+//https://api.meteo-concept.com/api/ephemeride/0?token=df67b5d9a4ad5c4d7edc7cb5bfd546524b5c69c768c28b951e0da9199128b388
 document.querySelector('#postalCode').addEventListener('input', function (){
     if(this.value.length == 5) {
         let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&type=commune-actuelle&fields=nom,code,codesPostaux&format=json&geometry=centre`;
